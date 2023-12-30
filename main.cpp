9bd130f6759a198007734cc2c8fb63d551295074
sajji <sajjadkazemi.e@gmail.com>
2023-12-30 14:19:59 -0500

    Update the code in this file to be compatible with the requirements using MyGraph class

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
         if (graph.edges.size() > 0)
             {
                 graph.getVertexCover();
+                graph.printVertexCover();
                 graph.resetEverything();
             }
         
