d769242163464e772a92b889c50a0abeb1082eaf
sajji <sajjadkazemi.e@gmail.com>
2023-12-30 14:02:18 -0500

    Put the base code in the file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
-int main(int argc, char **argv) {
+#include <iostream>
+#include "helpers.h"
+#include "MyGraph.h"
+
+int main(int argc, char **argv) 
+{
+    while (!std::cin.eof())
+    {
+        std::string line;
+        std::getline(std::cin, line);
+        std::string command = line_parser::get_command(line, graph);
+        line_parser::analyze_command(command, line, graph);    }
+    
  return 0;
 }
