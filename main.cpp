a2c23ffd0ddf1e003e63fbce308b12a7bcec43cc
sajji <sajjadkazemi.e@gmail.com>
2023-12-30 14:04:46 -0500

    Define graph as MyGraph to run the code

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 
 int main(int argc, char **argv) 
 {
+    MyGraph graph;
     while (!std::cin.eof())
     {
         std::string line;
         std::getline(std::cin, line);
         std::string command = line_parser::get_command(line, graph);
-        line_parser::analyze_command(command, line, graph);    }
+        line_parser::analyze_command(command, line, graph);    
+    }
     
  return 0;
 }
