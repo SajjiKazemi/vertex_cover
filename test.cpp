6a66f752a9844b107fd80d24f480447f9b4408e0
sajji <sajjadkazemi.e@gmail.com>
2023-12-30 15:01:52 -0500

    Add more tests to cover the recent change.

--- test.cpp
+++ test.cpp
@@ -88,7 +88,55 @@ TEST_CASE("A challenging one")
     std::streambuf* oldCout = std::cout.rdbuf(output.rdbuf());
     graph.getVertexCover();
     graph.printVertexCover();
+    graph.resetEverything();
+    graph.setSize(0);
     std::cout.rdbuf(oldCout);
     CHECK(output.str() == "5\n");
+    line = "E {<1,5>,<5,2>,<1,4>,<4,5>,<4,3>,<2,4>}";
+    std::ostringstream output2;
+    std::streambuf* oldCout2 = std::cout.rdbuf(output2.rdbuf());
+    command = line_parser::get_command(line, graph);
+    line_parser::analyze_command(command, line, graph);
+    std::cout.rdbuf(oldCout2);
+    CHECK(output2.str() == "Error: no vertices defined\n");
+}
+
+TEST_CASE("Final test case")
+{
+    MyGraph graph;
+    std::string line = "V 350";
+    std::string command = line_parser::get_command(line, graph);
+    line_parser::analyze_command(command, line, graph);
+    line = "E {<73,1>}";
+    command = line_parser::get_command(line, graph);
+    line_parser::analyze_command(command, line, graph);
+    std::ostringstream output;
+    std::streambuf* oldCout = std::cout.rdbuf(output.rdbuf());
+    graph.getVertexCover();
+    graph.printVertexCover();
+    graph.resetEverything();
+    graph.setSize(0);
+    std::cout.rdbuf(oldCout);
+    bool condition = (output.str() == "1\n" || output.str() == "73\n");
+    CHECK(condition == true);
+    line = "E {<1,5>,<5,2>,<1,4>,<4,5>,<4,3>,<2,4>}";
+    std::ostringstream output2;
+    std::streambuf* oldCout2 = std::cout.rdbuf(output2.rdbuf());
+    command = line_parser::get_command(line, graph);
+    line_parser::analyze_command(command, line, graph);
+    std::cout.rdbuf(oldCout2);
+    CHECK(output2.str() == "Error: no vertices defined\n");
+    line = "V 5";
+    command = line_parser::get_command(line, graph);
+    line_parser::analyze_command(command, line, graph);
+    line = "E {<1,5>,<5,2>,<1,4>,<4,5>,<4,3>,<2,4>}";
+    command = line_parser::get_command(line, graph);
+    line_parser::analyze_command(command, line, graph);
+    std::ostringstream output3;
+    std::streambuf* oldCout3 = std::cout.rdbuf(output3.rdbuf());
+    graph.getVertexCover();
+    graph.printVertexCover();
+    std::cout.rdbuf(oldCout3);
+    CHECK(output3.str() == "4 5\n");
 }
 
